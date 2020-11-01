dd71e1bebf7479b73ca846bbada490172dc40fe9
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2020-11-01 19:14:40 -0300

    test: ensure SignUpController returns an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,7 +2,7 @@ import { SignUpController } from "./signup";
 
 describe("SignUp Controller", () => {
   test("Should return 400 if no name is provided ", () => {
-    // sut : system under test
+    // sut: system under test
     const sut = new SignUpController();
     const httpRequest = {
       body: {
