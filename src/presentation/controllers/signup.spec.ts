49a979be319cdf3abeffd2a45675a4d97dbd0d91
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2020-11-03 11:16:46 -0300

    refactor: mode all error imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,10 @@
-import { InvalidParamError } from "../errors/invalid-param-error";
-import { MissingParamError } from "../errors/missing-param-error";
+import {
+  MissingParamError,
+  InvalidParamError,
+  ServerError,
+} from "../errors/index";
 import { SignUpController } from "./signup";
 import { EmailValidator } from "../protocols/email-validator";
-import { ServerError } from "../errors/server-error";
 
 interface SutTypes {
   sut: SignUpController;
