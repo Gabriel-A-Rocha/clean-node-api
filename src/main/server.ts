3cf45a05a284bea5ecd92aa62385beb11bc3c8c3
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2020-11-08 00:14:34 -0300

    feat: ensure server can parse json request bodies

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,5 @@
-import express from "express";
-
-const app = express();
+import app from "./config/app";
 
 app.listen(5050, () => {
   console.log("Server running at http://localhost:5050");
-});
-
-app.get("/", (req, res) => {
-  res.send("<p>Server running at http://localhost:5050</p>");
 });
